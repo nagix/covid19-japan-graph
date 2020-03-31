fdc367f6eaf85bd2cd93746c2749b3f86eb9be52
Akihiko Kusanagi <nagi@nagi-p.com>
2020-04-01 02:08:11 +0800

    Improve styles and interactions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2020 Akihiko Kusanagi
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ * More information about this project is available at:
+ *
+ *    https://github.com/nagix/covid19-japan-graph
+ */
+
 var DATA_URL = 'data';
 var DATA_FILE = 'data.json';
 var GRAPH_MARGIN = 20;
@@ -427,9 +447,10 @@ loadData('japan').then(function(patients) {
 			return (16 + d.nodes.length) + 'px';
 		})
 		.attr('stroke-width', function(d) {
-			return (16 + d.nodes.length) / 16;
+			return (16 + d.nodes.length) / 6;
 		})
 		.attr('text-anchor', 'middle')
+		.attr('stroke', '#aaa')
 		.attr('fill', '#aaa');
 
 	groupLabel.append('text')
@@ -440,7 +461,6 @@ loadData('japan').then(function(patients) {
 			return (16 + d.nodes.length) + 'px';
 		})
 		.attr('text-anchor', 'middle')
-		.attr('stroke', 'none')
 		.attr('fill', '#fff');
 
 	svg.selectAll("circle.legend").data([0, 0.25, 0.5, 0.75, 1])
